fd7eaf6069164444af2c1e3fa905ab7abb835ebc
Vikas Agarwal <35756021+vikasagarwal11@users.noreply.github.com>
2025-08-19 15:39:37 -0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { registerSW } from './sw-register';
-
+registerSW();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
 
-registerSW();
+
 
