3bfedab83400d8548ccfae257a8f7aa16cb48abc
Vikas Agarwal <35756021+vikasagarwal11@users.noreply.github.com>
2025-08-19 19:59:13 -0400

    Updated MediaUploadModal.tsx

--- src/components/media/MediaUploadModal.tsx
+++ src/components/media/MediaUploadModal.tsx
@@ -1,3 +1,4 @@
+import EventTypeahead from './EventTypeahead';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
